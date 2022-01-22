f2cc92d72773a00c3d94b0ada976e4c3328ac767
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-22 12:41:06 +0530

    [Shreya] UC3_Compareline

--- LineComparison/Cartesian.cs
+++ LineComparison/Cartesian.cs
@@ -4,45 +4,62 @@ using System.Text;
 
 namespace LineComparison
 {
-    class Equaltwopoints
-    {
-
-        // Function to check if they are identical
-        public static void idstrt(double a1, double b1,
-                          double c1, double a2,
-                          double b2, double c2)
-        {
-            if ((a1 / a2 == b1 / b2)
-                && (a1 / a2 == c1 / c2)
-                && (b1 / b2 == c1 / c2))
-                Console.WriteLine("The given straight"
-                + " lines are identical");
-
-            else
-                Console.WriteLine("The given straight"
-                    + " lines are not identical");
-        }
+	public class Compare
+	{
+		// Function to calculate length
+		double distance(int x1, int y1, int x2, int y2)
+		{
+			// Calculating Length
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+						Math.Pow(y2 - y1, 2) * 1.0);
+		}
 
 
-        public static void LineEqual()
-        {
-            //Line formula y= ax + bx + c
-            //Reading the co-efficent of two lines
-            Console.WriteLine("Please enter co-effcient a1");
-            double a1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter co-effcient b1 ");
-            double b1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter co-effcient c1");
-            double c1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter co-effcient a2");
-            double a2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter co-effcient b2 ");
-            double b2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter co-effcient c2");
-            double c2 = Convert.ToDouble(Console.ReadLine());
+		public void CompareLines()
+		{
+			// line1 co-orinates
+			Console.WriteLine("Please enter line1 ln1x1");
+			int ln1x1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line1 ln1y1");
+			int ln1y1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line1 ln1x2");
+			int ln1x2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line1 ln1y2");
+			int ln1y2 = Convert.ToInt32(Console.ReadLine());
 
-            // Call Method to Check constant ratio
-            idstrt(a1, b1, c1, a2, b2, c2);
-        }
-    }
+			//Length of line 1
+			double length1 = (Math.Round(distance(ln1x1, ln1y1, ln1x2, ln1y2)
+									* 100000.0) / 100000.0);
+			Console.WriteLine("Length of line1: " + length1);
+
+			//Line2 Co-orinates 
+			Console.WriteLine("Please enter line2 ln2x1");
+			int ln2x1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line2 ln2y1");
+			int ln2y1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line2 ln2x2");
+			int ln2x2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter line2 ln2y2");
+			int ln2y2 = Convert.ToInt32(Console.ReadLine());
+
+			//length of line 2
+			double length2 = (Math.Round(distance(ln2x1, ln2y1, ln2x2, ln2y2)
+									* 100000.0) / 100000.0);
+			Console.WriteLine("Length of line2: " + length2);
+
+			//Comparision of Length
+			if (length1 > length2)
+			{
+				Console.WriteLine("line1 is greater");
+			}
+			else if (length1 < length2)
+			{
+				Console.WriteLine("line2 is greater");
+			}
+			else
+			{
+				Console.WriteLine("lines are equal");
+			}
+		}
+	}
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LineComparison;
 Console.WriteLine("Hello, World!");
-Equaltwopoints.LineEqual();
+Compare compare = new Compare();
+compare.CompareLines();
